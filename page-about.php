ea814df3436c4329a8e30591807abd69dac7ac7b
Ricky Riyadi <rckryd.alt@gmail.com>
2025-03-08 17:28:38 +0700

    about: point

--- page-about.php
+++ page-about.php
@@ -11,11 +11,18 @@ get_header();
     'breadcrumbs' => [
       ['label' => 'トップ', 'url' => home_url()],
       ['label' => 'タカハタとは']
+    ],
+    'menu_items' => [
+        '3つの信念' => ['url' => '#', 'target' => ''],
+        '会社概要' => ['url' => '#', 'target' => ''],
+        'スタッフ紹介' => ['url' => '#', 'target' => ''],
+        'タカハタの歴史' => ['url' => '#', 'target' => ''],
+        'アクセス' => ['url' => '#', 'target' => '']
     ]
   ]);
   ?>
 
-  <section data-bg-color="yellow" class="about-top">
+  <section data-bg-color="black" class="about-top">
     <div class="wrapper">
       <h2 class="relative">
         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/who-is-takahata.svg" alt="WHO IS TAKAHATA">
@@ -32,6 +39,57 @@ get_header();
         また、専任のコンセルジュが<br class="md:hidden">ご相談から購入後のサポートまで丁寧に対応し、<br>
         快適なドライブ体験をお約束します。
       </p>
+    </div>
+  </section>
+
+  <section data-bg-color="yellow" class="about-point">
+    <div class="wrapper">
+      <div class="sc-title">
+        <h2>POINT</h2>
+        <p>我々の大切にしている3つの信念</p>
+      </div>
+
+      <div class="point-list pt-14 md:pt-16">
+        <div class="point-list__item">
+          <div class="point-list__num">01</div>
+          <div class="point-list__desc">
+            <h3>自動車・サービスの品質</h3>
+            <p>
+              安全性・機能・デザインすべてを兼ね備えた一台をご提案。<br>豊富な専門知識と熟練の経験を持つプロだけが在籍しており、お客様のご希望に合わせた<br class="max-md:hidden">最適な車選びをお手伝いします。
+            </p>
+          </div>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-point-01.svg" alt="Icon Point 01">
+        </div>
+        <div class="point-list__item">
+          <div class="point-list__num">02</div>
+          <div class="point-list__desc">
+            <h3>徹底的にニーズに寄り添う</h3>
+            <p>
+              車選び・保険・アフターサービスなどを、お客様一人に対して一人の担当が一貫して対応。<br>お客様のニーズに対して徹底的に耳を傾け、お客様が望むカーライフに沿ったご提案をい<br class="max-md:hidden">たします。
+            </p>
+          </div>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-point-02.svg" alt="Icon Point 02">
+        </div>
+        <div class="point-list__item">
+          <div class="point-list__num">03</div>
+          <div class="point-list__desc">
+            <h3>ホスピタリティを生む<br class="md:hidden">社員教育</h3>
+            <p>
+              お客様に信頼いただき最適なご提案ができるように、<br class="max-md:hidden">日々、サービススキルの向上に努めています。<br class="max-md:hidden">丁寧でわかりやすいご案内・ニーズを的確にとらえるヒアリング・細やかな心配りで、<br class="max-md:hidden">お客様一人ひとりに徹底したおもてなしをいたします。
+            </p>
+          </div>
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-point-03.svg" alt="Icon Point 03">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section data-bg-color="black" class="about-edu">
+    <div class="wrapper">
+      <div class="sc-title">
+        <h2>EDUCATION  BUSINESS</h2>
+        <p>教育事業</p>
+      </div>
     </div>
   </section>
 </main>
--- parts/title-section.php
+++ parts/title-section.php
@@ -1,25 +1,42 @@
 <?php
 $title = isset($args['title']) ? $args['title'] : 'PAGE TITLE';
-$breadcrumbs = isset($args['breadcrumbs']) ? $args['breadcrumbs'] : [];
+$breadcrumbs = isset($args['breadcrumbs']) ? $args['breadcrumbs'] : null; // Breadcrumb opsional
+$menu_items = isset($args['menu_items']) ? $args['menu_items'] : null; // Anchor opsional
 ?>
 
-<section class="s-title">
+<section class="s-title" data-bg-color="black">
     <h1><?php echo esc_html($title); ?></h1>
-    <?php if (!empty($breadcrumbs)) : ?>
+
+    <?php if ($breadcrumbs && !empty($breadcrumbs)) : ?>
         <div class="breadcrumb">
-          <ul>
-            <?php foreach ($breadcrumbs as $breadcrumb) : ?>
-                <li>
-                    <?php if (!empty($breadcrumb['url'])) : ?>
-                        <a href="<?php echo esc_url($breadcrumb['url']); ?>">
-                            <?php echo esc_html($breadcrumb['label']); ?>
-                        </a>
-                    <?php else : ?>
-                        <span><?php echo esc_html($breadcrumb['label']); ?></span>
-                    <?php endif; ?>
-                </li>
-            <?php endforeach; ?>
-          </ul>
+            <ul>
+                <?php foreach ($breadcrumbs as $breadcrumb) : ?>
+                    <li>
+                        <?php if (!empty($breadcrumb['url'])) : ?>
+                            <a href="<?php echo esc_url($breadcrumb['url']); ?>">
+                                <?php echo esc_html($breadcrumb['label']); ?>
+                            </a>
+                        <?php else : ?>
+                            <span><?php echo esc_html($breadcrumb['label']); ?></span>
+                        <?php endif; ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     <?php endif; ?>
 </section>
+
+<?php if ($menu_items && !empty($menu_items)) : ?>
+    <div class="anchor">
+        <ul>
+            <?php foreach ($menu_items as $label => $item) : ?>
+                <li>
+                    <a href="<?php echo esc_url($item['url']); ?>" 
+                       <?php echo !empty($item['target']) ? 'target="' . esc_attr($item['target']) . '"' : ''; ?>>
+                        <?php echo esc_html($label); ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
